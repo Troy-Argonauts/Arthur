9aadf7b995de1f25e3624ecd3322dc194935ddae
haste32767 <40278130+SolidityContract@users.noreply.github.com>
2022-01-29 00:38:34 -0500

    a

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.I_DriveIntake;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Intake;
 
--- src/main/java/frc/robot/commands/DT_CheesyDrive.java
+++ src/main/java/frc/robot/commands/DT_CheesyDrive.java
@@ -1,11 +1,7 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Robot;
-
-import java.util.Set;
 
 public class DT_CheesyDrive extends CommandBase {
 
