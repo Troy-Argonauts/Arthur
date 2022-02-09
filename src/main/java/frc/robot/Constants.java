5a22acbdb2692df9263a8cdd7386d878c11e00c4
DarkMG1 <64174035+DarkMG1@users.noreply.github.com>
2022-02-09 11:54:04 -0500

    Added Shooter Constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,6 +19,7 @@ public final class Constants {
     public static int DT_REAR_RIGHT = 3;
     public static int DT_REAR_LEFT = 4;
     public static int INTAKE = 5;
+    public static int SHOOTER = 6;
 
     public static int CONTROLLER_PORT = 0;
 }
