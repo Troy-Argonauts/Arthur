ce0dee5aa56ca053ee04de88efda1ea924ea0a32
DarkMG1 <64174035+DarkMG1@users.noreply.github.com>
2022-02-09 11:50:48 -0500

    Intake Adjustments

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,20 +1,15 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.commands.I_DriveIntake;
+import frc.robot.commands.I_Intake;
 
 public class Intake extends SubsystemBase {
 
-    private CANSparkMax intakeMotor = new CANSparkMax(Constants.INTAKE, CANSparkMax.MotorType.kBrushless);
-
-    public Intake() {
-        setDefaultCommand(new I_DriveIntake());
-    }
+    private final CANSparkMax intakeMotor = new CANSparkMax(Constants.INTAKE, CANSparkMax.MotorType.kBrushless);
 
     public void activate(double speed) {
-
+        intakeMotor.set(speed);
     }
 }
