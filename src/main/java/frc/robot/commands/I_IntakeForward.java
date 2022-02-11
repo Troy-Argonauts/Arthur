8f89abd6077d5e8398e2200c5bee662f0fa7607e
haste32767 <40278130+SolidityContract@users.noreply.github.com>
2022-02-11 18:41:19 -0500

    Intake Revision
    Split I_Intake Command to I_IntakeForward and I_IntakeBackward
    Changed Controller Triggers to Controller Shoulders
    
    Co-authored-by: akashsarada <95705632+akashsarada@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/I_IntakeForward.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+
+public class I_IntakeForward extends CommandBase {
+
+    public I_IntakeForward() {
+        addRequirements(Robot.getIntake());
+    }
+
+    @Override
+    public void execute() {
+        Robot.getIntake().excecuteForward();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Robot.getIntake().activate(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
