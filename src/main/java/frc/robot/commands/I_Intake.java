7626a23721b6efa53926e0013457e3bb97ee0430
haste32767 <40278130+SolidityContract@users.noreply.github.com>
2022-02-11 00:15:53 -0500

    a

--- src/main/java/frc/robot/commands/I_Intake.java
+++ src/main/java/frc/robot/commands/I_Intake.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
-import frc.robot.commands.I_Intake;
 
 public class I_Intake extends CommandBase {
 
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.commands.I_Intake;
 
 public class Intake extends SubsystemBase {
 
