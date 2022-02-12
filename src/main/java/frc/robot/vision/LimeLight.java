f5ab0e0a9bee50d3b7a10f519525c2b0e25c2a21
haste32767 <40278130+SolidityContract@users.noreply.github.com>
2022-02-12 17:04:25 -0500

    Limelight Additions
    Added LimeLight Functions
    
    Co-authored-by: akashsarada <95705632+akashsarada@users.noreply.github.com>

--- src/main/java/frc/robot/vision/LimeLight.java
+++ src/main/java/frc/robot/vision/LimeLight.java
@@ -1,4 +1,37 @@
 package frc.robot.vision;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Constants;
+
 public class LimeLight {
+
+    private static NetworkTable getTable() {
+        return NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    public static void start() {
+        getTable().getEntry("camMode").setNumber(0);
+    }
+
+    public static double getXAngle() {
+        return getTable().getEntry("tx").getDouble(0.0);
+    }
+
+    public static double getYAngle() {
+        return getTable().getEntry("ty").getDouble(0.0);
+    }
+
+    public static boolean contourFound() {
+        return getTable().getEntry("tv").getDouble(0.0) == 1;
+    }
+
+    public static double getContourArea() {
+        return getTable().getEntry("ta").getDouble(0.0);
+    }
+
+    public double distance() {
+        return Constants.LL_SHOOTERTOHUB_Y / Math.sin(getYAngle());
+    }
+    
 }
