52eae82284b00932ae75aa2ce681c2a91bff8576
akashsarada <95705632+akashsarada@users.noreply.github.com>
2022-02-16 18:35:58 -0500

    Intake Revisions V3
    More Intake Revisions:
    Added I_IntakeStop.java to stop the Intake
    Added holding A-Button to stop Intake

---
+++ src/main/java/frc/robot/commands/I_IntakeStop.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Robot;
+
+public class I_IntakeStop extends InstantCommand {
+  public I_IntakeStop() {
+    addRequirements(Robot.getIntake());
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    Robot.getIntake().disable();
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+}
