2820629b1f782755f11d075ecac196975152fcdc
DarkMG1 <bhat.chiragos@gmail.com>
2022-02-16 11:59:14 -0500

    Made Intake Motors Toggle

--- src/main/java/frc/robot/commands/I_IntakeToggle.java
+++ src/main/java/frc/robot/commands/I_IntakeToggle.java
@@ -3,21 +3,19 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
-public class I_IntakeForward extends CommandBase {
+public class I_IntakeToggle extends CommandBase {
 
-    public I_IntakeForward() {
+    public I_IntakeToggle() {
         addRequirements(Robot.getIntake());
     }
 
     @Override
     public void execute() {
-        Robot.getIntake().executeForward();
+        Robot.getIntake().toggle();
     }
 
     @Override
-    public void end(boolean interrupted) {
-        Robot.getIntake().activate(0);
-    }
+    public void end(boolean interrupted) {}
 
     @Override
     public boolean isFinished() {
