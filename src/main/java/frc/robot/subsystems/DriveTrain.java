88063625676b77588904adef605ab063e2554665
DarkMG1 <bhat.chiragos@gmail.com>
2022-04-01 18:38:31 -0400

    Added Gyro

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,6 +6,8 @@ import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -16,6 +18,7 @@ public class DriveTrain extends SubsystemBase {
      */
 
     private final TalonFX frontLeft, frontRight, rearLeft, rearRight;
+    private ADXRS450_Gyro gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
 
     /**
      * Sets the values of the frontLeft and frontRight motors, and creates local rear motors.
@@ -66,6 +69,8 @@ public class DriveTrain extends SubsystemBase {
         frontRight.setNeutralMode(NeutralMode.Coast);
         rearLeft.setNeutralMode(NeutralMode.Coast);
         rearRight.setNeutralMode(NeutralMode.Coast);
+
+        gyro.calibrate();
     }
 
     /**
